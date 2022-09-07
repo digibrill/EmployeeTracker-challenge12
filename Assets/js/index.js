7e1285766c09c2ab2c44bc81534cc6dbe588da21
JC Berry <jcharlesberry@gmail.com>
2022-09-07 10:56:33 -0700

    Lot of progress

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,8 +1,9 @@
 const inquirer = require('inquirer');
-const menu = require('inquirer-menu');
-const fs = require('fs');
+//const menu = require('inquirer-menu');
+//const fs = require('fs');
 const util = require('util');
 const mysql = require('mysql2');
+//const mysqlPromise = require('mysql2/promise');
 
 /*const Employee = require('./lib/employee');*/
 
@@ -25,230 +26,241 @@ let employeeManager = [];
 
 // add department, add to database
 let departmentName = [];
+let deptItemsConv = [];
 
 // add role, add to database
 let roleName = [];
 let roleSalary = [];
 let roleDepartment = [];
+let roleItemsConv = [];
 
 // add employee, add to database
-let employeeFirstName = [];
-let employeeLastName = [];
+//let employeeFirstName = [];
+//let employeeLastName = [];
 let employeeRole = [];
-let employeeManager = [];
+//let employeeManager = [];
 
 // select employee and update employee role, add to database
-let employeeRole = [];
+//let employeeRole = [];
 
-const startApp =  async () => {
+// Connect to db
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Mysqlz777@!',
+    database: 'employees'
+  },
+  console.log(`Connected to the employees database.`)
+);
+
+const startApp =  () => {
   inquirer.prompt([
     {
         name: "choose_action", //choose_position
         type: "list",
         message: "What do you want to do?",
-        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
-    },
-    {
-        name: "add_employee",
-        type: "confirm",
-        message: "Do you want to add another employee? (No, to quit and save)",
-    },
-    // WHEN I choose to view all departments; THEN I am presented with a formatted table showing department names and department ids
-/*        when (answers){
-          return answers.add_employee;
-        }*/
-
-/*    {
-      type: 'input',
-      message: 'What is your name?',
-      name: 'managerName',
-    },
-    {
-      type: 'input',
-      message: 'What is your employee ID?',
-      name: 'managerId',
-    },
-    {
-      type: 'input',
-      message: 'What is your email address?',
-      name: 'managerEmail',
-    },
-    {
-      type: 'input',
-      message: 'What is your office number?',
-      name: 'managerOfficeNumber',
-    },*/
-  {
-    name: "choose_position",
-        type: "list",
-        message: "Which type of employee?",
-        choices: [ "Engineer", "Intern"],
-        when (answers){
-          return answers.add_employee === true;
-        }
-  }
-  
-  ]).then((managerAnswers) => {
-    //console.log(answers.managerName);
-    if(!managerAnswers.add_employee){
-      let mgr = new Manager(managerAnswers.managerName, managerAnswers.managerEmail, managerAnswers.managerId, managerAnswers.managerOfficeNumber,'Manager');
-      positions.push(mgr);
-      continueQuiz('exit');
-    }else if(managerAnswers.managerName){
-      let mgr = new Manager(managerAnswers.managerName, managerAnswers.managerEmail, managerAnswers.managerId, managerAnswers.managerOfficeNumber,'Manager');
-      positions.push(mgr);
-      continueQuiz(managerAnswers.choose_position);
+        choices: ["View all employees or add employee", "View all roles or add role", "View all departments or add department", "Update employee role, department, or manager"],
+    }
+  ]).then((chooseActionAnswers) => {
+    if(chooseActionAnswers.choose_action === "View all employees or add employee"){
+        actionOptions('viewEmployees');
+    }else if(chooseActionAnswers.choose_action === "View all roles or add role"){
+        actionOptions('viewRoles');
+    }else if(chooseActionAnswers.choose_action === "View all departments or add department"){
+        actionOptions('viewDepartments');
+    }else if(chooseActionAnswers.choose_action === "Update employee role, department, or manager"){
+        actionOptions('updateEmployee');
     }else{}
-});
-
-const askForContinue = async () => {
-  inquirer.prompt([
-    /*{
-      name: "add_employee",
-      type: "confirm",
-      message: "Do you want to add another employee? (No, to quit and save)",
-    },*/
-    {
-      name: "choose_action", //choose_position
-      type: "list",
-      message: "What do you want to do?",
-      choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
-      when (answers){
-        return answers.add_employee;
-      }
-    }
-  ]).then((answers) => {
-    if(!answers.add_employee){
-
-      continueQuiz('exit');
-    }else{
-      continueQuiz(answers.choose_position);
-    }
   });
-};
 
-const continueQuiz = async (start) => {
-  if(start === 'Engineer'){
-    inquirer.prompt([
+const actionOptions = (actionChoice) => {
+  if(actionChoice === 'viewDepartments'){
+    //SQL get departments
+    db.promise().query('SELECT * FROM departments').then((result) => {
+      console.table(result[0]);
+    }).then(() => {
+      inquirer.prompt([
     {
-                type: 'input',
-                message: 'What is the engineer\'s name?',
-                name: 'engineerName',
-    },  
-    {
-                type: 'input',
-                message: 'What is the engineer\'s employee ID?',
-                name: 'engineerId',
+      name: "add_department",
+      message: "Here are the current departments. Do you want to add one?",
+      type: "confirm",
     },
     {
-                type: 'input',
-                message: 'What is the engineer\'s email address?',
-                name: 'engineerEmail',
-    },
-    {
-                type: 'input',
-                message: 'What is the engineer\'s Github?',
-                name: 'engineerGithub',
+      name: "department_name",
+      message: "Department name?",
+      type: "input",
+      when: (answers) => answers.add_department === true
     }
-    ]).then((engineerAnswers) => {
-      const egr = new Engineer(engineerAnswers.engineerName, engineerAnswers.engineerEmail, engineerAnswers.engineerId, engineerAnswers.engineerGithub,'Engineer');
-      positions.push(egr);
-      askForContinue();
+    ]).then((deptAnswers) => {
+      db.promise().query(`INSERT INTO departments (dept_name) VALUES ('${deptAnswers.department_name}')`,{})
+      db.promise().query('SELECT * FROM departments',{});
+    }).then(() => {
+      console.log('Thank you for using the Employee Tracker');
+      startApp();
     });
-  }else if(start === 'Intern'){
-    inquirer.prompt([
-    {
-                type: 'input',
-                message: 'What is the intern\'s name?',
-                name: 'internName',
-    },
-    {
-                type: 'input',
-                message: 'What is the intern\'s employee ID?',
-                name: 'internId',
-    },
-    {
-                type: 'input',
-                message: 'What is the intern\'s email address?',
-                name: 'internEmail',
-    },
-    {
-                type: 'input',
-                message: 'Where does the intern go to school?',
-                name: 'internSchool',
-    }
-    ]).then((internAnswers) => {
-        const int = new Intern(internAnswers.internName, internAnswers.internEmail, internAnswers.internId, internAnswers.internSchool,'Intern');
-        positions.push(int);
-        askForContinue();
     });
-  }else if (start === "exit"){
-      let employeeCards = '';
-      for(let i=0; i < positions.length; i++){
-        if(positions[i].name){
-          let newCard = `<div class="card"><div class="card-body"><div class="card-title">${positions[i].title}</div>
-          <div><span class="card-title">${positions[i].name}</span><br>
-          <span class="card-subtitle">${positions[i].id}</span><br>
-          <span class="empEmail"><a href="mailto:${positions[i].email}" target="_blank" class="card-link">${positions[i].email}</a></span><br>`;
-          if(positions[i].officeNumber){
-            newCard += `<span class="empOfficeNumber" class="card-link">Office Number: ${positions[i].officeNumber}</span></div></div></div>`;
-          }else if(positions[i].gitHub){
-            newCard += `<span class="engGithub">Engineer's GitHub: <a href="https://${positions[i].gitHub}" class="card-link" target="_blank">${positions[i].gitHub}</a></span></div></div></div>`;
-          }else if(positions[i].school){
-            newCard += `<span class="intSchool" class="card-link">Intern's School: ${positions[i].school}</span></div></div></div>`;
-          }
-          employeeCards += newCard;
+  }else if(actionChoice === 'viewRoles'){
+    /*db.promise().query('SELECT * FROM departments').then( (result) => {
+      console.table(result[0]);
+    }).then(() => {
+      inquirer.prompt([*/
+    //SQL get departments
+    let deptRoleItemsConv = [];
+      //deptItems = Object.values(deptIdResult[0]);
+      //deptItems.forEach(deptItem => deptItemsConv.push(deptItem.dept_name));
+    //db.promise().query(`INSERT INTO departments (dept_name) VALUES ('${deptAnswers.department_name}')`,{})
+    //let deptItems;
+    db.promise().query('SELECT dept_name FROM departments').then( deptsForRoles => {
+      deptRoleItems = Object.values(deptsForRoles[0]);
+      deptRoleItems.forEach(deptRoleItem => {
+        deptRoleItemsConv.push(deptRoleItem.dept_name);
+        //console.log(deptRoleItemsConv);
+      })
+    });
+    db.promise().query('SELECT role FROM roles').then( listRoles => {
+      console.table(listRoles[0]);
+    }).then( (roleResult) => {
+      inquirer.prompt([
+        {
+          name: "add_role",
+          message: "Here are the current roles. Do you want to add one?",
+          type: "confirm",
+        },
+        {
+          name: "enter_role",
+          message: "Role name?",
+          type: "input",
+          when: (answers) => answers.add_role === true
+        },
+        {
+          name: "enter_salary",
+          message: "Role salary?",
+          type: "input",
+          when: (answers) => answers.add_role === true
+        },
+        {
+          name: "enter_role_dept",
+          message: "Choose role's department.",
+          type: "list",
+          choices: deptRoleItemsConv,
+          when: (answers) => answers.add_role === true
         }
+        ]).then((roleAnswers) => {
+          let id;
+          db.promise().query(`SELECT id FROM departments WHERE dept_name='${roleAnswers.enter_role_dept}'`).then( result => {
+            [[{id}]] = result;
+        }).then( roleAnswersNext => {
+            
+            //db.promise().query(`INSERT INTO roles (role, salary, dept_id) VALUES ('${roleAnswersNext.enter_role}', ${roleAnswersNext.enter_salary}, ${id}`);
+          console.log(roleAnswersNext);
+        })
+      })
+    });
+  }else if(actionChoice === 'viewEmployees'){
+      //SQL get departments
+      db.query('SELECT * FROM roles', function (err, results) {
+        roleItems = Object.values(results);
+        roleItems.forEach((roleItem) => {
+          roleItemsConv.push(roleItem.role);
+        });
+      });
+      //SQL get employees
+      db.query('SELECT * FROM employees', function (err, results) {
+        //console.clear();
+        //console.table(results);
+      });
+      
+      inquirer.prompt([
+      {
+        name: "add_employee",
+        message: "Here are the current employees. Do you want to add one?",
+        type: "confirm"
+      },
+      {
+        name: "enter_employee_fname",
+        message: "Employee first name?",
+        type: "input",
+        when: (answers) => answers.add_employee === true
+      },
+      {
+        name: "enter_employee_lname",
+        message: "Employee last name?",
+        type: "input",
+        when: (answers) => answers.add_employee === true
+      },
+      {
+        name: "enter_employee_role",
+        message: "Employee role?",
+        type: "list",
+        choices: roleItemsConv,
+        when: (answers) => answers.add_employee === true
+      },
+      {
+        name: "enter_employee_mgr",
+        message: "Employee manager?",
+        type: "input",
+        when: (answers) => answers.add_employee === true
       }
-      fs.writeFile(`index.html`, `<!DOCTYPE html>
-            <html lang="en">
-              <head>
-              <meta charset="UTF-8">
-              <title>Acme Corp's Employee page</title>
-              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-              <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-              <link
-                rel="stylesheet"
-                href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-              />
-              <link
-                rel="stylesheet"
-                href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-                integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-                crossorigin="anonymous"
-              />
-              <link
-                href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap"
-                rel="stylesheet"
-              />
-              <link href="./Assets/styles/normalize.css" rel="stylesheet" />
-              <link href="./Assets/styles/styles.css" rel="stylesheet" />
-            </head>
-            <body>
-              <header class="jumbotron">
-                <h1>
-                Our Great Team!
-                </h1>
-              </header>
-              <main id="cardDiv">
-                <h2>More About Us:</h2>
-                ${employeeCards}
-              </main>
-              <footer>
-                An Unassociated Press Publication
-              </footer>
-            </body>
-            </html>`, (err) =>
-            err ? console.error(err) : console.log('Team members saved!'));
-      };
+      ])
+      .then((employeeAnswers) => {
+        //SQL get employees
+        db.query(`SELECT id FROM roles WHERE role='${employeeAnswers.enter_employee_role}'`, function (err, result) {
+          const [{id}] = result;
+          db.query(`INSERT INTO employees (first_name, last_name, role, manager) VALUES ('${employeeAnswers.enter_employee_fname}','${employeeAnswers.enter_employee_lname}',${id},'${employeeAnswers.enter_employee_mgr}')`, function (err, results) {
+          });
+        });
+        db.query('SELECT * FROM employees', function (err, results) {
+          //console.table(results);
+        });
+    }).then(() => {
+      console.log('Thank you for using the Employee Tracker');
+      startApp();
+    });
+  }else if(actionChoice === 'updateEmployee'){
+      //SQL get departments
+      db.query('SELECT * FROM roles', function (err, results) {
+        roleItems = Object.values(results);
+        roleItems.forEach((roleItem) => {
+        roleItemsConv.push(roleItem.role);
+        });
+      });
+      inquirer.prompt([
+        {
+          name: "update_employee",
+          type: "input",
+          message: "Which employee do you want to update? (enter their ID)"
+        },
+        {
+          name: "update_manager",
+          type: "input",
+          message: "Which manager should they have?"
+        },
+        {
+          name: "update_role",
+          type: "list",
+          message: "Which role should they have?",
+          choices: roleItemsConv
+        }
+      ]).then((employeeAnswers) => {
+        //SQL get employees
+        console.clear();
+        db.query(`SELECT id FROM roles WHERE role='${employeeAnswers.update_role}'`, function (err, result) {
+          const [{id}] = result;
+          db.query(`UPDATE employees SET role=${id}, manager='${employeeAnswers.update_manager}' WHERE id=${employeeAnswers.update_employee}`, function (err, results) {
+          });
+        });
+        db.query('SELECT * FROM employees', function (err, results) {
+          console.clear();
+          console.table(results);
+        });
+      }).then(() => {
+        console.log('Thank you for using the Employee Tracker');
+        startApp();
+      });
+  }else{
   }
+}
 }
 
 startApp();
-
-
-
-//startMenu();
-/**
-EXTRA CREDIT validation to ensure that user input provided is in the proper expected format.
-*/

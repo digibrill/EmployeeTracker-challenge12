95ec449fb1eb7cff77c3de219851f6d703202e86
JC Berry <jcharlesberry@gmail.com>
2022-09-07 18:36:42 -0700

    Turning in version 1

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,47 +1,7 @@
+/* Imports */
 const inquirer = require('inquirer');
-//const menu = require('inquirer-menu');
-//const fs = require('fs');
 const util = require('util');
 const mysql = require('mysql2');
-//const mysqlPromise = require('mysql2/promise');
-
-/*const Employee = require('./lib/employee');*/
-
-let positions = [];
-
-//View all roles
-let jobTitle = []; 
-let roleId = [];
-let department = [];
-let salary = [];
-
-// view all employees/employee data in a formatted table
-let employeeId = [];
-let employeeFirstName = [];
-let employeeLastName = [];
-let employeeTitle = [];
-let employeeDepartment = [];
-let employeeSalary = [];
-let employeeManager = [];
-
-// add department, add to database
-let departmentName = [];
-let deptItemsConv = [];
-
-// add role, add to database
-let roleName = [];
-let roleSalary = [];
-let roleDepartment = [];
-let roleItemsConv = [];
-
-// add employee, add to database
-//let employeeFirstName = [];
-//let employeeLastName = [];
-let employeeRole = [];
-//let employeeManager = [];
-
-// select employee and update employee role, add to database
-//let employeeRole = [];
 
 // Connect to db
 const db = mysql.createConnection(
@@ -54,10 +14,11 @@ const db = mysql.createConnection(
   console.log(`Connected to the employees database.`)
 );
 
+/* Menu */
 const startApp =  () => {
   inquirer.prompt([
     {
-        name: "choose_action", //choose_position
+        name: "choose_action",
         type: "list",
         message: "What do you want to do?",
         choices: ["View all employees or add employee", "View all roles or add role", "View all departments or add department", "Update employee role, department, or manager"],
@@ -74,12 +35,14 @@ const startApp =  () => {
     }else{}
   });
 
+/* Main function */
 const actionOptions = (actionChoice) => {
   if(actionChoice === 'viewDepartments'){
     //SQL get departments
     db.promise().query('SELECT * FROM departments').then((result) => {
       console.table(result[0]);
     }).then(() => {
+      // Departments questions
       inquirer.prompt([
     {
       name: "add_department",
@@ -93,15 +56,18 @@ const actionOptions = (actionChoice) => {
       when: (answers) => answers.add_department === true
     }
     ]).then((deptAnswers) => {
-      db.promise().query(`INSERT INTO departments (dept_name) VALUES ('${deptAnswers.department_name}')`,{})
-      db.promise().query('SELECT * FROM departments',{});
-    }).then(() => {
+      //Insert new department
+      db.promise().query(`INSERT INTO departments (dept_name) VALUES ('${deptAnswers.department_name}')`);
+      db.promise().query('SELECT * FROM departments').then((result) => {
+        console.table(result[0]);
+      }).then(() => {
       console.log('Thank you for using the Employee Tracker');
       startApp();
+      });
     });
-    });
+  });
   }else if(actionChoice === 'viewRoles'){
-    //SQL get departments
+    //SQL get departments for roles
     let deptRoleItemsConv = [];
     db.promise().query('SELECT dept_name FROM departments').then( deptsForRoles => {
       deptRoleItems = Object.values(deptsForRoles[0]);
@@ -113,6 +79,7 @@ const actionOptions = (actionChoice) => {
     db.promise().query('SELECT role FROM roles').then( listRoles => {
       console.table(listRoles[0]);
     }).then( (roleResult) => {
+      // role questions
       inquirer.prompt([
         {
           name: "add_role",
@@ -138,19 +105,24 @@ const actionOptions = (actionChoice) => {
           choices: deptRoleItemsConv,
           when: (answers) => answers.add_role === true
         }
+        // Insert new role
         ]).then((roleAnswers) => {
           let id;
           db.promise().query(`SELECT id FROM departments WHERE dept_name='${roleAnswers.enter_role_dept}'`).then( result => {
             [[{id}]] = result;
-            db.promise().query(`INSERT INTO roles (role, salary, dept_id) VALUES ('${roleAnswers.enter_role}', ${roleAnswers.enter_salary}, ${id})`).then();
-            //;
-          });
-        }).then(() => {
-          console.log('Thank you for using the Employee Tracker');
-          startApp();
+            db.promise().query(`INSERT INTO roles (role, salary, dept_id) VALUES ('${roleAnswers.enter_role}', ${roleAnswers.enter_salary}, ${id})`);
+          }).then(() => {
+            db.promise().query('SELECT role FROM roles').then( listRoles => {
+              console.table(listRoles[0]);
+            }).then(() => {  
+              console.log('Thank you for using the Employee Tracker');
+              startApp();
+            });
         });
+      });
     });
   }else if(actionChoice === 'viewEmployees'){
+    //SQL get roles for employees
     let rolesForEmployeesConv = [];
     db.promise().query('SELECT role FROM roles').then( rolesForEmployees => {
       roleItems = Object.values(rolesForEmployees[0]);
@@ -161,6 +133,7 @@ const actionOptions = (actionChoice) => {
       db.promise().query('SELECT * FROM employees').then( employeeList => { 
         console.table(employeeList[0]);
       }).then(() => {
+        // employee questions
         inquirer.prompt([
           {
             name: "add_employee",
@@ -192,13 +165,18 @@ const actionOptions = (actionChoice) => {
             type: "input",
             when: (answers) => answers.add_employee === true
           }
+          // insert new employee
         ]).then((employeeAnswers) => {
           db.promise().query(`SELECT id FROM roles WHERE role='${employeeAnswers.enter_employee_role}'`).then((result) => {
             const [{id}] = result[0];
             db.promise().query(`INSERT INTO employees (first_name, last_name, manager, role) VALUES ('${employeeAnswers.enter_employee_fname}','${employeeAnswers.enter_employee_lname}','${employeeAnswers.enter_employee_mgr}',${id})`);
           }).then(() => {
-            console.log('Thank you for using the Employee Tracker');
-            startApp();
+            db.promise().query('SELECT * FROM employees').then((results) => {
+              console.table(results[0]);
+            }).then(() => {
+              console.log('Thank you for using the Employee Tracker');
+              startApp();
+            });
           });
         });
       });
@@ -206,17 +184,17 @@ const actionOptions = (actionChoice) => {
     });
   }else if(actionChoice === 'updateEmployee'){
     let roleUpdateItemsConv = [];
-    //SQL get departments
+    //SQL get roles for employee
     db.promise().query('SELECT role FROM roles').then((updateRoleResults) => {
       roleUpdateItems = Object.values(updateRoleResults[0]);
       roleUpdateItems.forEach((roleUpdateItem) => {
         roleUpdateItemsConv.push(roleUpdateItem.role);
       });
-      //console.log(roleUpdateItemsConv);
     }).then(() => {
       db.promise().query('SELECT * FROM employees').then( employeeList => { 
         console.table(employeeList[0]);
       }).then(() => {
+        // update employee questions
         inquirer.prompt([
         {
           name: "update_employee",
@@ -234,24 +212,25 @@ const actionOptions = (actionChoice) => {
           message: "Which role should they have?",
           choices: roleUpdateItemsConv
         }
+        // Update employee
         ]).then((employeeAnswers) => {
-          //SQL get employees
+          //SQL get roles for updating
           db.promise().query(`SELECT id FROM roles WHERE role='${employeeAnswers.update_role}'`).then((result) => {
             const [{id}] = result[0];
-            //console.log(id);
+            //update employee
             db.promise().query(`UPDATE employees SET role=${id}, manager='${employeeAnswers.update_manager}' WHERE id=${employeeAnswers.update_employee}`);
             db.promise().query('SELECT * FROM employees').then((results) => {
               console.table(results[0]);
             }).then(() => {
               console.log('Thank you for using the Employee Tracker');
               startApp();
+              });
             });
           });
         });
       });
-    });
-  }else{
+    }else{
+    }
   }
-}
 }
 startApp();
